044629c7ee5100aa35b27c4e88ef3898139678a6
Taiki Hayashi <hayashitaiki@team-lab.com>
2019-06-05 23:23:41 +0900

    リトライ処理の追加

--- staticcheck/entrypoint.sh
+++ staticcheck/entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 set -e
 cd "${GO_ACTION_WORKING_DIR:-.}"
-if [ ! -e go.mod ]; then
-	go mod init
-fi
-go mod download
+
+if [ ! -e go.mod ]; then go mod init; fi
+for i in {1..3}; do
+	go mod download && break
+done
+if [ $? -ne 0 ]; then exit 1; fi
 
 set +e
 OUTPUT=$(sh -c "staticcheck ${FLAGS} ./... $*" 2>&1)
@@ -33,4 +35,6 @@ PAYLOAD=$(echo '{}' | jq --arg body "${COMMENT}" '.body = $body')
 COMMENTS_URL=$(cat /github/workflow/event.json | jq -r .pull_request.comments_url)
 curl -s -S -H "Authorization: token ${GITHUB_TOKEN}" --header "Content-Type: application/json" --data "${PAYLOAD}" "${COMMENTS_URL}" > /dev/null
 
-exit ${SUCCESS}
+# dry run, ignore SUCCESS
+# exit ${SUCCESS}
+exit 0
